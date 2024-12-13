c7060ce615ab317ff59d3e8762e14f17b0bbb534
wogus <ind07152@naver.com>
2024-12-13 15:00:18 +0900

    feat(LineRepository): 지하철 노선 관련 기능 추가
    - 노선에서 역 삭제 기능을 추가한다.
    - 노선에 역 추가 기능을 추가한다.
    - 노선에 대한 검증을 추가한다.
    - 모든 노선의 이름을 반환하는 기능을 추가한다.
    - 특정 노선의 역들을 반환하는 기능을 추가한다.

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -1,22 +1,57 @@
 package subway.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import subway.exception.ErrorMessage;
+import subway.exception.StationException;
 
 public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
 
-    public static List<Line> lines() {
-        return Collections.unmodifiableList(lines);
+    public List<String> lines() {
+        return lines.stream().map(Line::getName).collect(Collectors.toList());
     }
 
-    public static void addLine(Line line) {
-        lines.add(line);
+    public void deleteLineByName(String name) {
+        if (!lines.removeIf(line -> Objects.equals(line.getName(), name))) {
+            throw StationException.from(ErrorMessage.NO_LINE);
+        }
     }
 
-    public static boolean deleteLineByName(String name) {
-        return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    public boolean hasLine(String line) {
+        return lines.stream().anyMatch(next -> {
+            return next.getName().equals(line);
+        });
     }
+
+    public void addLine(String line) {
+        if (hasLine(line)) {
+            throw StationException.from(ErrorMessage.ALREADY_EXIST_LINE);
+        }
+        lines.add(new Line(line));
+    }
+
+    public void addStation(String lineName, Station station) {
+        Line findLine = lines.stream().filter(line -> {
+            return line.getName().equals(lineName);
+        }).findFirst().orElseThrow(() -> StationException.from(ErrorMessage.NO_LINE));
+        findLine.addStation(station);
+    }
+
+    public void addStationAt(String lineName, int index, Station station) {
+        Line findLine = lines.stream().filter(line -> {
+            return line.getName().equals(lineName);
+        }).findFirst().orElseThrow(() -> StationException.from(ErrorMessage.NO_LINE));
+        findLine.addStationAt(station, index);
+    }
+
+    public List<String> getStation(String lineName) {
+        Line findLine = lines.stream().filter(line -> {
+            return line.getName().equals(lineName);
+        }).findFirst().orElseThrow(() -> StationException.from(ErrorMessage.NO_LINE));
+        return findLine.getStations();
+    }
+
 }
