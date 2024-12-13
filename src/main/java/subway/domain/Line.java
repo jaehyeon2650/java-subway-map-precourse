51b3a9be6d4a80a7620b1525dc733f72b4440e3b
wogus <ind07152@naver.com>
2024-12-13 14:43:22 +0900

    feat(Line): 지하철 노선 관련 기능 추가
    - 노선에서 역 삭제 기능을 추가한다.
    - 노선에 역 추가 기능을 추가한다.
    - 노선에 대한 검증을 추가한다.
    - 노선에 있는 역의 이름을 반환하는 기능을 추가한다.

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,9 +1,17 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import subway.exception.ErrorMessage;
+import subway.exception.StationException;
+
 public class Line {
     private String name;
+    private final List<Station> stations = new ArrayList<>();
 
     public Line(String name) {
+        Validator.validateNameLength(name);
         this.name = name;
     }
 
@@ -11,5 +19,53 @@ public class Line {
         return name;
     }
 
-    // 추가 기능 구현
+    public void addStation(Station station) {
+        Validator.validateDuplicateStation(stations, station);
+        stations.add(station);
+    }
+
+    public void addStationAt(Station station, int index) {
+        Validator.validateDuplicateStation(stations, station);
+        stations.add(index, station);
+    }
+
+    public void deleteStation(Station station) {
+        Validator.validateDelete(stations, station);
+        stations.remove(station);
+    }
+
+    public List<String> getStations() {
+        return stations.stream().map(Station::getName).collect(Collectors.toList());
+    }
+
+    private static class Validator {
+        public static void validateDelete(List<Station> stations, Station station) {
+            validateHasStation(stations, station);
+            canDelete(stations);
+        }
+
+        public static void validateNameLength(String name) {
+            if (name.length() < 2) {
+                throw StationException.from(ErrorMessage.INVALID_LINE_NAME_LENGTH);
+            }
+        }
+
+        public static void validateDuplicateStation(List<Station> stations, Station station) {
+            if (stations.contains(station)) {
+                throw StationException.from(ErrorMessage.INVALID_DUPLICATE_STATION_IN_LINE);
+            }
+        }
+
+        private static void validateHasStation(List<Station> stations, Station station) {
+            if (!stations.contains(station)) {
+                throw StationException.from(ErrorMessage.NO_STATION_IN_LINE);
+            }
+        }
+
+        private static void canDelete(List<Station> stations) {
+            if (stations.size() <= 2) {
+                throw StationException.from(ErrorMessage.CANT_REMOVE_STATION);
+            }
+        }
+    }
 }
